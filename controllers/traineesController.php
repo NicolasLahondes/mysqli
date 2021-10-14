3dbad05fe114d69886e983cf745b1a4249020612
Lahondès <lahondes.n@gmail.com>
2021-10-14 09:37:57 +0200

    added connexion bdd and errors

--- controllers/traineesController.php
+++ controllers/traineesController.php
@@ -3,51 +3,11 @@
 include 'models/trainees.php';
 
 
-// $selectMe = "SELECT * FROM student WHERE name LIKE '%LAHONDES%'";
-// $queryMe = mysqli_query($conn, $selectMe);
-// $queryParse = $queryMe->fetch_assoc();
 
-var_dump($_POST);
+$results = $bddConn->query("SELECT * from student");
 
-// Add
+$fetchedResults = $results->fetchAll();
 
-if (!empty($_POST) && !empty($_POST['name']) && !empty($_POST['firstname']) && !empty($_POST['birthdate'])) {
-    $addMarc = "INSERT INTO student (name, firstname, birthdate) VALUES ('" . $_POST['name'] . "','" . $_POST['firstname'] . "','" . $_POST['birthdate'] . "')";
-
-    if (mysqli_query($conn, $addMarc)) {
-        echo "Records inserted successfully.";
-        header("Location:index.php?action=eleves");
-    } else {
-        echo "ERROR: Could not able to execute $addMarc. " . mysqli_error($conn);
-    }
-}
-
-
-
-// Delete
-// $remove = "DELETE FROM student WHERE firstname LIKE '%marc%'";
-
-// if (mysqli_query($conn, $remove)) {
-//     echo "Records inserted successfully.";
-// } else {
-//     echo "ERROR: Could not able to execute $remove. " . mysqli_error($conn);
-// }
-
-
-// Update
-// $update = "UPDATE student SET name = 'Remplacement', firstname = 'de Remplacement Village', birthdate = '1045-10-12' WHERE firstname LIKE '%marc%'";
-
-// if (mysqli_query($conn, $update)) {
-//     echo "Records inserted successfully.";
-// } else {
-//     echo "ERROR: Could not execute $update. " . mysqli_error($conn);
-// }
-
-
-
-// Display all requests
-$sql = "SELECT * FROM student";
-$results = mysqli_query($conn, $sql);
-$resultsCheck = mysqli_num_rows($results);
+var_dump($fetchedResults);
 
 include 'views/traineesView.php';
--- dataBase/dataConnexion.php
+++ dataBase/dataConnexion.php
@@ -1,12 +1,20 @@
-<?php 
+<?php
 
 $dbBooks = "localhost";
 $dbUsername = "root";
 $dbPassword = "nevolepasmesdonnéescindy";
 $dbName = "mysqlphplink";
 
-$conn = mysqli_connect($dbBooks,$dbUsername,$dbPassword,$dbName);
+// $conn = mysqli_connect($dbBooks,$dbUsername,$dbPassword,$dbName);
 
-
-
-?>
+try {
+    //Setting bdd connexion
+    $bddConn = new PDO("mysql:host=$dbBooks;dbname=$dbName", $dbUsername, $dbPassword);
+    // If error generate exception
+    $bddConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    // Get the idea of error
+    echo $e->getCode();
+    // Display entire error msg
+    die('Erreur :' . $e->getMessage());
+}
